4944c0ca9dbd58213bfd054e7e666c009a6a9f52
Pankaj Baid <baidpankaj567@gmail.com>
2025-07-21 10:25:27 +0530

    rate limit

--- app.js
+++ app.js
@@ -34,8 +34,8 @@ const generalLimiter = rateLimit({
 });
 
 const authLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // limit each IP to 5 requests per windowMs for auth routes
+  windowMs: 1 * 60 * 1000, // 15 minutes
+  max: 1000, // limit each IP to 5 requests per windowMs for auth routes
   message: 'Too many authentication attempts, please try again later.'
 });
 
