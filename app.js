6f00ee08ed038f46065a26573e11f464ea596c8c
Pankaj Baid <baidpankaj567@gmail.com>
2025-07-21 10:19:23 +0530

    cors

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(helmet());
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
     ? ['https://your-frontend-domain.com'] 
-    : ['http://localhost:5174', 'http://localhost:5173'],
+    : ['http://localhost:5174', 'http://localhost:5173','https://rasa-yatra.vercel.app'],
   credentials: true
 }));
 
